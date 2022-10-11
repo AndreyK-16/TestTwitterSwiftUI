2283226aedd01ee3cd1d8847dd0b60c0712770d0
AndreyKaldyaev <59248096+AndreyK-16@users.noreply.github.com>
2022-10-11 21:49:35 +0300

    Initial Commit

---
+++ TestTwitterSwiftUI/TestTwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTwitterSwiftUIApp.swift
+//  TestTwitterSwiftUI
+//
+//  Created by Andrey Kaldyaev on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestTwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
