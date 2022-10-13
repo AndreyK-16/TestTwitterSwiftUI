eca532df299d64122a86fb89bf35ddf681bc0708
AndreyKaldyaev <59248096+AndreyK-16@users.noreply.github.com>
2022-10-13 19:18:24 +0300

    added Explore page UI

---
+++ TestTwitterSwiftUI/Core/Explore/View/ExploreView.swift
@@ -0,0 +1,37 @@
+//
+//  ExploreView.swift
+//  TestTwitterSwiftUI
+//
+//  Created by Andrey Kaldyaev on 11.10.2022.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScrollView {
+                    LazyVStack {
+                        ForEach(0...25, id: \.self) { _ in
+                            NavigationLink {
+                                ProfileView()
+                            } label: {
+                                UserRowView()
+                            }
+
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
