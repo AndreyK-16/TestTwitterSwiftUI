59d97a43043069bc3db09929e17238ab5ef46fbe
AndreyKaldyaev <59248096+AndreyK-16@users.noreply.github.com>
2022-11-02 22:32:02 +0300

    added custom SearchBar and added filtering users

--- TestTwitterSwiftUI/Core/Explore/ViewModel/ExploreViewModel.swift
+++ TestTwitterSwiftUI/Core/Explore/ViewModel/ExploreViewModel.swift
@@ -9,12 +9,28 @@ import Foundation
 
 class ExploreViewModel: ObservableObject {
     @Published var users = [User]()
+    @Published var searchText = ""
     let service = UserService()
+    
+    var searchableUsers: [User] {
+        if searchText.isEmpty {
+            return users
+        } else {
+            let lowercasedQuery = searchText.lowercased()
+            
+            return users.filter({
+                $0.username.contains(lowercasedQuery) ||
+                $0.fullname.lowercased().contains(lowercasedQuery)
+            })
+        }
+    }
     
     init() {
         fetchUsers()
     }
     
+    /// Получение списка юзеров из хранилища в Firebase.
+    /// The function is use to fetch an array of users from Firebase
     func fetchUsers() {
         service.fetchUsers { users in
             self.users = users
